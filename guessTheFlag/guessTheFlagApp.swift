4991f7bdb4fa53e67a4c0c83245d48d7ac07898c
Joanna Ye <96999397+JoannaYe51@users.noreply.github.com>
2024-01-30 21:28:25 -0500

    upload the application

---
+++ guessTheFlag/guessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessTheFlagApp.swift
+//  guessTheFlag
+//
+//  Created by yqw on 2022/11/2.
+//
+
+import SwiftUI
+
+@main
+struct guessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
